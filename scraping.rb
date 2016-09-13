b48ece69d9c539e07eb86bec4123f4aaee177779
scottibe <bewick.scott@gmail.com>
2016-09-13 13:20:48 -0400

    Done.

--- scraping.rb
+++ scraping.rb
@@ -2,6 +2,6 @@ require 'nokogiri'
 require 'open-uri'
 
 
-doc = Nokogiri::HTML(open("http://flatironschool.com/"))
+doc = Nokogiri::HTML(open("http://flatironschool.com"))
 puts doc.css("site-header__supernav__inner") 
 
